e6fd5b9ec74ff4d99584edee05e6110358f924b9
HookPlate <41370249+HookPlate@users.noreply.github.com>
2021-12-29 16:43:08 +0000

    Initial Commit

---
+++ EmojiArt/EmojiArtApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiArtApp.swift
+//  EmojiArt
+//
+//  Created by robin tetley on 29/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct EmojiArtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
