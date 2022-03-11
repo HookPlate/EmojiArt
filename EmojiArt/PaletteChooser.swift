c70ccaac3f412cdc3b7fe1fecce9bbf0f0fb0469
HookPlate <41370249+HookPlate@users.noreply.github.com>
2022-03-11 09:59:30 +0000

    Adding the ability to cycle through the palettes at the bottom

--- EmojiArt/PaletteChooser.swift
+++ EmojiArt/PaletteChooser.swift
@@ -14,14 +14,44 @@ struct PaletteChooser: View {
     
     @EnvironmentObject var store: PaletteStore
     
+    @State private var chosenPaletteIndex = 0
+    
     
     var body: some View {
-        let palette = store.palette(at: 2)
+        HStack {
+            paletteControlButton
+            body(for: store.palette(at: chosenPaletteIndex))
+        }
+        .clipped()
+    }
+    
+    var paletteControlButton: some View {
+        Button {
+            withAnimation {
+                chosenPaletteIndex = (chosenPaletteIndex + 1) % store.palettes.count
+            }
+            
+        } label: {
+            Image(systemName: "paintpalette")
+        }
+        .font(emojiFont)
+    }
+    
+    func body(for palette: Palette) -> some View {
         HStack {
             Text(palette.name)
             scrollingEmojisView(emojis: palette.emojis)
                 .font(.system(size: emojiFontSize))
         }
+        .id(palette.id)
+        .transition(rollTransition)
+    }
+    
+    var rollTransition: AnyTransition {
+        AnyTransition.asymmetric(
+            insertion: .offset(x: 0, y: emojiFontSize),
+            removal: .offset(x: 0, y: -emojiFontSize)
+        )
     }
 }
 
